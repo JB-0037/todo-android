2098fee8115a137ec9c374ebf7503ee821f5eaae
Jean-Baptiste DAULAUS <daulaus@et.intechinfo.fr>
2025-03-10 23:12:00 +0100

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { StyleProp, StyleSheet, Text, TextInput, TextStyle, View } from "react-native";
 
 export default function Index() {
+  const [todo, setTodo] = useState<string>();
+
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Ceci est un texte</Text>
+    <View>
+      <View>
+        <TextInput
+          style={styles.input}
+          onChangeText={(e: string) => setTodo(e)}
+          value={todo}
+          maxLength={50}
+        />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  input: {
+    borderColor: "black",
+    borderWidth: 1,
+    borderStyle: "solid",
+  }
+});
