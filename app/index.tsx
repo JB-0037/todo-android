4d357aa56922bd6748da4db5e5547e704a8b5004
Jean-Baptiste DAULAUS <82393153+JB-0037@users.noreply.github.com>
2025-03-11 19:10:26 +0100

    Update index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -2,16 +2,26 @@ import { Stack } from "expo-router";
 import { useState } from "react";
 import { Keyboard, Pressable, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
 
+type Todo = {
+  id: string;
+  label: string;
+  isCompleted: boolean;
+};
+
 export default function Index() {
   const [todo, setTodo] = useState<string>();
-  const [todos, setTodos] = useState<string[]>([]);
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   function addTodo() {
     if (todo) {
-      setTodos([...todos, todo]);
+      setTodos([...todos, { id: Date.now().toString(), label: todo, isCompleted: false }]);
       setTodo("");
       Keyboard.dismiss();
     }
+  }
+
+  function deleteTodo(id: string) {
+    setTodos(todos.filter((todo) => todo.id !== id));
   }
 
   return (
@@ -37,10 +47,17 @@ export default function Index() {
         </Pressable>
       </View>
 
-      <ScrollView style={styles.todos}>
-        {todos.map((todo, index) => (
-          <Text key={index}>{todo}</Text>
-        ))}
+      <ScrollView style={styles.scrollview}>
+        <View style={styles.todos}>
+          {todos.map((todo) => (
+            <View style={styles.todo} key={todo.id}>
+              <Text >{todo.label}</Text>
+              <Pressable onPress={() => deleteTodo(todo.id)}>
+                <Text style={{ fontWeight: "bold" }}>X</Text>
+              </Pressable>
+            </View>
+          ))}
+        </View>
       </ScrollView>
     </View>
   );
@@ -71,7 +88,22 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center"
   },
+  todo: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    borderWidth: 2,
+    borderRadius: 8,
+    borderColor: "black",
+    backgroundColor: "white",
+    padding: 10
+  },
+  scrollview: {
+    height: "90%",
+  },
   todos: {
-    height: "90%"
+    display: "flex",
+    flexDirection: "column",
+    gap: 10
   }
 });
